a8f09a828dbc88b2f0460dc50568be4e965c131e
jbmitchell02 <jbm8efn@virginia.edu>
2023-02-01 16:45:13 -0500

    Demonstration and python code completed.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # The Relationship Between the Multi-Period Binomial Model and the Black-Scholes Model
+Author: Mitch Mitchell (jbm8efn)
+*Note: The goal of this project was to use the equivalencies between the multi-period binomial model and Black-Scholes model to build a deeper understanding of both techniques, as well as geometric brownian motion in general. That's why I chose to only use functions provided by python itself and the math library.*
+
+Files:
+
+* convergence_demonstration.ipynb: Walkthrough of the project using call options
+* option_pricing.py: Functions to calculate European option prices (call and put) using the code written in covergence_demonstration.ipynb
--- option_pricing.py
+++ option_pricing.py
@@ -1,8 +1,7 @@
 # Author: Mitch Mitchell (jbm8efn@virginia.edu)
-# Functions for calculating the price of a European call option
+# Functions for calculating the price of European options
 
 import math
-from scipy.stats import norm
 
 # S: Current stock price
 # K: Strike Price
@@ -10,7 +9,7 @@ from scipy.stats import norm
 # T: Time to expiration (in years)
 # sigma: Standard deviation of stock returns
 # r_f: Risk-free rate
-def binomial_price(S, K, n, T, sigma, r_f):
+def binomial_price(S, K, n, T, sigma, r_f, type={'call', 'put'}):
     u = pow(math.e, sigma * math.sqrt(T / n))
     d = 1 / u
     R = pow(math.e, r_f * T / n)
@@ -26,32 +25,57 @@ def binomial_price(S, K, n, T, sigma, r_f):
             df[j+1][i] = temp
         temp = df[0][i] * u
         height += 1
-    for i in range(n+1):
-        if df[i][n] > K:
-            df[i][n] -= K
-        else:
-            df[i][n] = 0
-    height = n
-    for i in reversed(range(n)):
-        for j in reversed(range(height)):
-            expected = (1 / R) * (q * df[j][i+1] + q_c * df[j+1][i+1])
-            df[j][i] = expected
-        height -= 1
-    return df[0][0]
+    if type == 'call':
+        for i in range(n+1):
+            if df[i][n] > K:
+                df[i][n] -= K
+            else:
+                df[i][n] = 0
+        height = n
+        for i in reversed(range(n)):
+            for j in reversed(range(height)):
+                expected = (1 / R) * (q * df[j][i+1] + q_c * df[j+1][i+1])
+                df[j][i] = expected
+            height -= 1
+        return df[0][0]
+    elif type == 'put':
+        for i in range(n+1):
+            if df[i][n] < K:
+                df[i][n] = K - df[i][n]
+            else:
+                df[i][n] = 0
+        height = n
+        for i in reversed(range(n)):
+            for j in reversed(range(height)):
+                expected = (1 / R) * (q * df[j][i+1] + q_c * df[j+1][i+1])
+                df[j][i] = expected
+            height -= 1
+        return df[0][0]
+
+# z: z-score
+def cdf(z):
+    p = 1 + math.erf(z / math.sqrt(2.0))
+    return (p / 2)
 
 # S: Current stock price
 # K: Strike Price
 # T: Time to expiration (in years)
 # sigma: Standard deviation of stock returns
 # r_f: Risk-free rate
-def black_scholes_price(S, K, T, sigma, r_f):
+def black_scholes_price(S, K, T, sigma, r_f, type={'call', 'put'}):
     d1 = math.log(S / K) + T * (r_f + (pow(sigma, 2) / 2))
     d1 /= (sigma * math.sqrt(T))
     d2 = d1 - (sigma * math.sqrt(T))
-    N1 = norm.cdf(d1)
-    N2 = norm.cdf(d2)
-    return (S * N1 - K * pow(math.e, -r_f * T) * N2)
+    N1 = cdf(d1)
+    N2 = cdf(d2)
+    C = (S * N1 - K * pow(math.e, -r_f * T) * N2)
+    if type == 'call':
+        return C
+    elif type == 'put':
+        return (C - S + K * pow(math.e, -r_f * T))
 
+
+# Main Method
 if __name__ == '__main__':
     S = 100
     K = 100
@@ -59,7 +83,9 @@ if __name__ == '__main__':
     T = 1
     sigma = 0.1
     r_f = 0.01
-    bin_price = binomial_price(S, K, n, T, sigma, r_f)
-    bs_price = black_scholes_price(S, K, T, sigma, r_f)
-    print('The', n, 'period binomial model price is $', bin_price)
-    print('The Black-Scholes model price is $', bs_price)
+    type = 'call'
+    bin_price = binomial_price(S, K, n, T, sigma, r_f, type)
+    bs_price = black_scholes_price(S, K, T, sigma, r_f, type)
+    print('European', type, 'option prices:')
+    print(n, 'period binomial model: $', bin_price)
+    print('Black-Scholes model: $', bs_price)
